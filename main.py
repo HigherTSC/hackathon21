88bade383fe8fe4c967bfcdb9a34cf8ca91eaeb0
ionut <ionutmanasia1910@gmail.com>
2021-06-07 23:42:05 +0100

    some date studd

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from datetime import date
+from datetime import date, timedelta
 
 today = date.today()
 start_date = date.fromisocalendar(2020, 24, 3)
-end_date = date.fromisocalendar(2021, 24, 3)
 
 goal_list = {}
-goal_index = 0
 
 
-def store_goal(start_date, end_date, amount_to_save, daily_pledge):
-    global goal_index
-    goal_list.update({goal_index: [start_date, end_date, amount_to_save, daily_pledge]})
-    goal_index = goal_index + 1
+def store_goal(name, start_date, amount_to_save, daily_pledge):
+    end_date = start_date + timedelta(days=round(amount_to_save / daily_pledge))
+    goal_list.update({name: [start_date, end_date, amount_to_save, daily_pledge]})
 
-store_goal(start_date,end_date,1000,10)
-print(repr(goal_list[0]))
+
+def end_streak(goal):
+    global goal_list
+    del goal_list[goal]
+
+def streak(goal):
+    global goal_list
+    current_streak = today - goal_list[goal][0]
+    return current_streak
+
+store_goal("car",start_date, 1000, 10)
+store_goal("vacation", start_date, 1000, 10)
+end_streak("car")
+print(repr(goal_list["vacation"]))
+print(streak("vacation"))
